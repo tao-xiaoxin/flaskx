728ddab502cb5b74152ccd3a6d6a809990cfd868
haloworker <haloworker@hotmail.com>
2024-06-24 15:02:50 +0800

    revise ：修改hello CBV

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -1,9 +1,13 @@
 from flask import Blueprint
+from flask import views
+from .views import index, IndexView
 
 hello = Blueprint('hello', __name__)
 
-
-
-
-
 hello.add_url_rule(rule='/', view_func=index, endpoint='index')
+
+
+def init_blueprints(app):
+    # CBV路由注册,as_view( )中必须传 name, name是该路由用于反向解析时的别名
+    app.add_url_rule('/', view_func=IndexView.as_view(name='index2'),
+                     defaults={'k1': 'hello word!'}, strict_slashes=False)
--- apps/hello/views.py
+++ apps/hello/views.py
@@ -1,2 +1,34 @@
+from flask import views
+
+
 def index():
     return 'Hello Word!'
+
+
+def auth(func):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        print("auth")
+        return res
+
+    return wrapper
+
+
+def login(func):
+    def wrapper(*args, **kwargs):
+        res = func(*args, **kwargs)
+        print("login")
+        return res
+
+
+# 继承MethodView
+class IndexView(views.MethodView):
+    methods = ['GET', "POST"]  # 限制请求方式
+    decorators = [auth, login]  # 添加装饰器,执行顺序自上而下
+
+    def get(self, k1):
+        print(k1)
+        return 'GET 请求!'
+
+    def post(self):
+        return 'POST 请求!'
